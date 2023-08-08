5daa9cd10cfb6ccbd0c17de1dacd867f3a3e475b
mrlom <mrlomka407@mail.ru>
2023-08-08 21:56:40 +0300

    update: edit ALLOWED_HOSTS

--- RateOnline/RateOnline/settings.py
+++ RateOnline/RateOnline/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-683!c64006p-t@w%zl05$l2l3=&6z+6gucl35_u5b7%^m$uh2o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
