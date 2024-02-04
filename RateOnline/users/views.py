f1ec98e14a5a697421eaeb8e27a061cd71221bcd
mrlom <mrlomka407@mail.ru>
2024-02-04 11:26:23 +0300

    fix check_code in views.py

--- RateOnline/users/views.py
+++ RateOnline/users/views.py
@@ -1,13 +1,13 @@
 import random
 
 from django.db.models import Q
-import smsc_api as sms
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, UpdateView
 from django.contrib import messages
 from users.models import User
+import smsc_api as sms
 from .forms import UploadImageForm
 
 
@@ -16,82 +16,86 @@ def check_phone(phone: str):
         return False
     if not phone.isnumeric():
         return False
-    if phone[0] not in ['7', '8']:
+    if phone[0] not in ["7", "8"]:
         return False
     return True
 
 
 class LoginIn(TemplateView):
-    template_name = 'users/index.html'
+    template_name = "users/index.html"
 
     def post(self, request):
-        phone = request.POST.get('phone')
+        phone = request.POST.get("phone")
         if check_phone(phone):
-            user = User.objects.filter(Q(phone_number=phone) | Q(phone_number=phone.replace('8', '7', 1))).first()
+            user = User.objects.filter(
+                Q(phone_number=phone) | Q(phone_number=phone.replace("8", "7", 1))
+            ).first()
             if user is None:
-                messages.error(request, 'Такого пользователя нет')
+                messages.error(request, "Такого пользователя нет")
                 return super().get(request)
             name_user = user.first_name
             random_number = str(random.randint(1000, 9999))
             token = sms.get_token()
-            print('token=' + token)
+            print("token=" + token)
             message_id = sms.send_sms(token, phone, random_number, name_user)
-            print('messageId=' + message_id)
+            print("messageId=" + message_id)
             status = sms.check_status(token, message_id)
-            print('status=' + status)
+            print("status=" + status)
             print(random_number)
             user.set_password(random_number)
             user.save()
-            return redirect(reverse_lazy('confirmation') + f'?phone={phone}')
-        messages.error(request, 'Номер введён неверно')
+            return redirect(reverse_lazy("confirmation") + f"?phone={phone}")
+        messages.error(request, "Номер введён неверно")
         return super().get(request)
 
 
 class CheckCode(TemplateView):
-    template_name = 'users/confirmation.html'
+    template_name = "users/confirmation.html"
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data['phone'] = self.request.GET.get('phone')
+        data["phone"] = self.request.GET.get("phone")
         return data
 
     def post(self, request):
-        phone = request.POST.get('phone')
-        password = request.POST.get('password')
-        user = User.objects.filter(phone_number=phone).first()
+        phone = request.POST.get("phone")
+        password = request.POST.get("password")
+        user = User.objects.filter(
+            Q(phone_number=phone) | Q(phone_number=phone.replace("8", "7", 1))
+        ).first()
         if user is None:
-            messages.error(request, 'Такого пользователя нет')
+            messages.error(request, "Такого пользователя нет")
             return super().get(request)
         if user.check_password(password):
             login(request, user)
-            return redirect(reverse_lazy('photo_selection'))
-        messages.error(request, 'Код введён неверно')
+            return redirect(reverse_lazy("photo_selection"))
+        messages.error(request, "Код введён неверно")
         return super().get(request)
 
 
 class PhotoUpload(UpdateView):
-    template_name = 'users/photo-selection.html'
+    template_name = "users/photo-selection.html"
     form_class = UploadImageForm
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         if self.request.user.is_staff:
-            data['user_request'] = 'staff'
+            data["user_request"] = "staff"
         else:
-            data['user_request'] = None
+            data["user_request"] = None
         return data
 
     def get(self, request, *args, **kwargs):
         if request.user.image:
             if self.request.user.is_staff:
-                return redirect(reverse_lazy('referee_page'))
-            return redirect(reverse_lazy('master_page'))
+                return redirect(reverse_lazy("referee_page"))
+            return redirect(reverse_lazy("master_page"))
         return super().get(request, *args, **kwargs)
 
     def get_success_url(self):
         if self.request.user.is_staff:
-            return reverse_lazy('referee_page')
-        return reverse_lazy('master_page')
+            return reverse_lazy("referee_page")
+        return reverse_lazy("master_page")
 
     def get_object(self, queryset=None):
         return self.request.user
